0bb0977a1c966616674300502e80a419be28c4ba
Emanuele Mule <100977128+EmaMule@users.noreply.github.com>
2024-10-17 23:14:56 +0200

    maybe solved a problem

--- MMLFN/checkpoint.py
+++ MMLFN/checkpoint.py
@@ -2,7 +2,22 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import pytorch_lightning as pl
 import os
 
-# @title Implementation ModelCheckpoint
+# # @title Implementation ModelCheckpoint
+# class MyModelCheckpoint(ModelCheckpoint):
+#     def __init__(self, config, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.config = config
+
+#     def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+#         if not self.dirpath:
+#             raise ValueError("dirpath must be specified in ModelCheckpoint")
+#         file_path = os.path.join(os.path.dirname(self.dirpath), "config.txt")
+#         with open(file_path, 'w') as f:
+#             f.write(self.config)
+#             f.write('\n')
+
+#         return super().on_train_start(trainer, pl_module)
+
 class MyModelCheckpoint(ModelCheckpoint):
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -11,9 +26,22 @@ class MyModelCheckpoint(ModelCheckpoint):
     def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         if not self.dirpath:
             raise ValueError("dirpath must be specified in ModelCheckpoint")
-        file_path = os.path.join(os.path.dirname(self.dirpath), "config.txt")
-        with open(file_path, 'w') as f:
+        
+        # Save the config file once at the beginning of training
+        config_file_path = os.path.join(os.path.dirname(self.dirpath), "config.txt")
+        with open(config_file_path, 'w') as f:
             f.write(self.config)
             f.write('\n')
 
-        return super().on_train_start(trainer, pl_module)
+        super().on_train_start(trainer, pl_module)
+
+    def on_save_checkpoint(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", checkpoint: dict) -> dict:
+        # Set a new directory for each epoch
+        epoch_dir = os.path.join(self.dirpath, f"epoch_{trainer.current_epoch}")
+        os.makedirs(epoch_dir, exist_ok=True)
+        
+        # Update the dirpath to the new epoch-specific directory
+        self.dirpath = epoch_dir
+        self.filename = f"{pl_module.__class__.__name__}_epoch={trainer.current_epoch}"
+        
+        return super().on_save_checkpoint(trainer, pl_module, checkpoint)
--- MMLFN/models/generic_model.py
+++ MMLFN/models/generic_model.py
@@ -52,8 +52,8 @@ class MultiBranchModel(pl.LightningModule):
         loss = self.compute_loss(embeddings, grd_emb, sat_emb)
         top_1 = top_k_rank_accuracy(grd_emb, sat_emb, k=1)
 
-        self.log('train_top1', top_1, on_step=True, on_epoch=True, prog_bar=True)
-        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True)
+        self.log('train_top1', top_1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
         return loss
 
@@ -64,7 +64,7 @@ class MultiBranchModel(pl.LightningModule):
         self.grd_features_val.append(grd_emb)
         self.sat_features_val.append(sat_emb)
         loss = self.compute_loss(embeddings, grd_emb, sat_emb)
-        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
 
     def on_validation_epoch_end(self):
@@ -85,15 +85,15 @@ class MultiBranchModel(pl.LightningModule):
       top_10_inverse = top_k_rank_accuracy(grd_features_val, sat_features_val, k=10, inverse=True)
       top_percent1_inverse = top_k_rank_accuracy(grd_features_val, sat_features_val, k=percent1, inverse=True)
 
-      self.log('val_top1', top_1, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('val_top5', top_5, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('val_top10', top_10, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('val_top1%', top_percent1, on_step=False, on_epoch=True, prog_bar=True)
+      self.log('val_top1', top_1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('val_top5', top_5, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('val_top10', top_10, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('val_top1%', top_percent1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
-      self.log('val_top1_inv', top_1_inverse, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('val_top5_inv', top_5_inverse, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('val_top10_inv', top_10_inverse, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('val_top1%_inv', top_percent1_inverse, on_step=False, on_epoch=True, prog_bar=True)
+      self.log('val_top1_inv', top_1_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('val_top5_inv', top_5_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('val_top10_inv', top_10_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('val_top1%_inv', top_percent1_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
       self.grd_features_val.clear()
       self.sat_features_val.clear()
@@ -128,15 +128,15 @@ class MultiBranchModel(pl.LightningModule):
       top_10_inverse = top_k_rank_accuracy(grd_features_test, sat_features_test, k=10, inverse=True)
       top_percent1_inverse = top_k_rank_accuracy(grd_features_test, sat_features_test, k=percent1, inverse=True)
 
-      self.log('test_top1', top_1, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('test_top5', top_5, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('test_top10', top_10, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('test_top1%', top_percent1, on_step=False, on_epoch=True, prog_bar=True)
+      self.log('test_top1', top_1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('test_top5', top_5, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('test_top10', top_10, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('test_top1%', top_percent1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
-      self.log('test_top1_inv', top_1_inverse, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('test_top5_inv', top_5_inverse, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('test_top10_inv', top_10_inverse, on_step=False, on_epoch=True, prog_bar=True)
-      self.log('test_top1%_inv', top_percent1_inverse, on_step=False, on_epoch=True, prog_bar=True)
+      self.log('test_top1_inv', top_1_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('test_top5_inv', top_5_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('test_top10_inv', top_10_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+      self.log('test_top1%_inv', top_percent1_inverse, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
       self.grd_features_test.clear()
       self.sat_features_test.clear()
