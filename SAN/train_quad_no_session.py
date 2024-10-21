fef33e2dd5f15544c1c4dd5b3994e1a958202fd7
Matteo Pannacci <52663658+MatteoPannacci@users.noreply.github.com>
2024-10-21 15:11:53 +0200

    Update train_quad_no_session.py

--- SAN/train_quad_no_session.py
+++ SAN/train_quad_no_session.py
@@ -45,6 +45,7 @@ loss_weight = 10.0
 learning_rate_val = 1e-4
 keep_prob_val = 0.8 # (not used)
 keep_prob = 0.8 # (not used)
+combination_type = 'concat' # concat, sum
 
 print("SETTED PARAMETERS: ")
 print("Train ground FOV: {}".format(train_grd_FOV))
@@ -183,12 +184,16 @@ def train(start_epoch=0):
                     grd_features, grdseg_features, sat_features, satseg_features = model([batch_grd, batch_grdseg, batch_sat_polar, batch_satseg])
                 
                     # feature extraction and concatenation
-                    ### ADD SMART COMBINATION THROUGH FULLY CONNECTED LAYER?
-                    grd_features = tf.concat([grd_features, grdseg_features], axis=-1)
-                    grd_features = tf.nn.l2_normalize(grd_features, axis=[1, 2, 3])
-                    sat_features = tf.concat([sat_features, satseg_features], axis=-1)
-                    # sat_features is normalized after cropping
+                    if combination_type == 'concat':
+                        grd_features = tf.concat([grd_features, grdseg_features], axis=-1)                        
+                        sat_features = tf.concat([sat_features, satseg_features], axis=-1)                        
+                    else combination_type == 'sum':
+                        grd_features += np.resize(grdseg_features, grd_features.shape)
+                        sat_features += np.resize(satseg_features, sat_features.shape)
 
+                    grd_features = tf.nn.l2_normalize(grd_features, axis=[1, 2, 3])
+                    # sat_features is normalized after cropping
+                    
                     # Compute correlation and distance matrix
                     sat_matrix, grd_matrix, distance, orien = processor.VGG_13_conv_v2_cir(sat_features,grd_features)
 
