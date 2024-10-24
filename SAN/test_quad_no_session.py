1a3cda6bf1758db15db6b36d33c52023403a3a06
Matteo Pannacci <matteo.pannacci@gmail.com>
2024-10-24 22:38:26 +0200

    Update test_quad_no_session.py

--- SAN/test_quad_no_session.py
+++ SAN/test_quad_no_session.py
@@ -1,11 +1,8 @@
 import os
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 from cir_net_FOV_mb import *
-
-# Import its own InputData
-from polar_input_data_quad import InputDataQuad
-
+from polar_input_data_quad import InputData
 from VGG_no_session import *
 import tensorflow as tf
 from tensorflow import keras
@@ -16,45 +13,50 @@ from PIL import Image
 import scipy.io as scio
 from numpy import fft
 
-#tf.compat.v1.disable_eager_execution()
+
 tf.compat.v1.enable_eager_execution()
 parser = argparse.ArgumentParser(description='TensorFlow implementation.')
 
-parser.add_argument('--start_epoch', type=int, help='from epoch', default=0)
-parser.add_argument('--number_of_epoch', type=int, help='number_of_epoch', default=100)
 
+# Parser
+parser.add_argument('--start_epoch', type=int, help='from epoch', default=0)
+parser.add_argument('--number_of_epoch', type=int, help='number_of_epoch', default=30)
 parser.add_argument('--train_grd_noise', type=int, help='0~360', default=360)
 parser.add_argument('--test_grd_noise', type=int, help='0~360', default=0)
-
 parser.add_argument('--train_grd_FOV', type=int, help='70, 90, 180, 360', default=360)
 parser.add_argument('--test_grd_FOV', type=int, help='70, 90, 180, 360', default=360)
-
+parser.add_argument('--name', type=str, default='unnamed')
+parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--acc_size', type=int, default=4)
 args = parser.parse_args()
 
-# Save parameters --------------------------------------- #
-start_epoch = args.start_epoch
+
+# Data Parameters
 train_grd_noise = args.train_grd_noise
 test_grd_noise = args.test_grd_noise
 train_grd_FOV = args.train_grd_FOV
 test_grd_FOV = args.test_grd_FOV
-number_of_epoch = args.number_of_epoch
-loss_type = 'l1'
-batch_size = 8
-is_training = True
-loss_weight = 10.0
-learning_rate_val = 1e-5
-keep_prob_val = 0.8
-keep_prob = 0.8
-dimension = 4
 
-print("SETTED PARAMETERS: ")
-print("Train ground FOV: {}".format(train_grd_FOV))
-print("Train ground noise: {}".format(train_grd_noise))
-print("Test ground FOV: {}".format(test_grd_FOV))
-print("Test ground noise: {}".format(test_grd_noise))
-print("Number of epochs: {}".format(number_of_epoch))
-print("Learning rate: {}".format(learning_rate_val))
-# -------------------------------------------------------- #
+# Model Parameters
+model_save_name = args.name
+combination_type = 'sum' # concat, sum
+grd_c = 16
+grdseg_c = 8
+sat_c = 16
+satseg_c = 8
+
+# Training Parameters
+start_epoch = args.start_epoch
+number_of_epoch = args.number_of_epoch
+batch_size = args.batch_size
+accumulation_size = args.acc_size
+loss_type  = 'triplet' # (unused)
+loss_weight = 10.0
+optimizer_type = 'adam' # adam, adamw
+learning_rate_val = 1e-4
+weight_decay = 0.004
+
+
 
 def validate(dist_array, topK):
     accuracy = 0.0
@@ -71,7 +73,8 @@ def validate(dist_array, topK):
     return accuracy
 
 
-def compute_loss(dist_array):
+
+def compute_loss_triplet(dist_array):
 
     pos_dist = tf.linalg.tensor_diag_part(dist_array)
     pair_n = batch_size * (batch_size - 1.0)
@@ -87,36 +90,73 @@ def compute_loss(dist_array):
     loss = (loss_g2s + loss_s2g) / 2.0
     return loss
 
-def train(start_epoch=14):
+
+
+def compute_loss_InfoNCE(sat_descriptor, grd_descriptor, logit_scale = 10.0):
+    
+    logits_sat_descriptor = logit_scale*sat_descriptor@grd_descriptor.T
+    labels = tf.range(tf.shape(logits_sat_descriptor)[0], dtype=tf.int64) #check the [0], could be wrong
+    loss_g2s = keras.ops.categorical_crossentropy(labels, logits_sat_descriptor, from_logits = True)
+
+    logits_grd_descriptor = logits_sat_descriptor.T
+    loss_s2g = keras.ops.categorical_crossentropy(labels, logits_grd_descriptor, from_logits = True)
+
+    loss = (loss_g2s + loss_s2g) / 2.0
+    return loss
+
+
+
+def train(start_epoch=0):
     '''
     Train the network and do the test
     :param start_epoch: the epoch id start to train. The first epoch is 0.
     '''
+
+    width = int(train_grd_FOV / 360 * 512)
+    
     # import data
-    input_data = InputDataQuad()
-    width = int(test_grd_FOV / 360 * 512)
+    input_data = InputData()
+    processor = ProcessFeatures()    
+
+    # choose loss function
+    if loss_type == 'triple':
+        compute_loss = compute_loss_triplet
+    else:
+        raise Exception("Loss not implemented!")        
 
     # Define the optimizer
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate_val)
-
-    # Siamese-like network branches
-    grdNet = VGGModel(tf.keras.Input(shape=(None, None, 3)),'_grd', out_channels=12)
-    grdSegNet = VGGModel(tf.keras.Input(shape=(None, None, 3)),'_grdseg', out_channels=4)
-    satNet = VGGModelCir(tf.keras.Input(shape=(None, None, 3)),'_sat', out_channels=12)
-    satSegNet = VGGModelCir(tf.keras.Input(shape=(None, None, 3)),'_satseg', out_channels=4)
+    if optimizer_type == 'adam':
+        optimizer = tf.keras.optimizers.Adam(
+            learning_rate=learning_rate_val
+        )
+    elif optimizer_type == 'adamw':
+        optimizer = tf.keras.optimizers.experimental.AdamW(
+            learning_rate=learning_rate_val,
+            weight_decay=weight_decay
+        )        
     
-    processor = ProcessFeatures()
-
+    # Siamese-like network branches
+    grdNet = VGGModel(tf.keras.Input(shape=(None, None, 3)),'_grd', out_channels=grd_c, freeze=True)
+    grdSegNet = VGGModel(tf.keras.Input(shape=(None, None, 3)),'_grdseg', out_channels=grdseg_c, freeze=True)
+    satNet = VGGModelCir(tf.keras.Input(shape=(None, None, 3)),'_sat', out_channels=sat_c, freeze=True)
+    satSegNet = VGGModelCir(tf.keras.Input(shape=(None, None, 3)),'satseg', out_channels=satseg_c, freeze=True)
+ 
     # Full Model
     model = Model(
          inputs=[grdNet.model.input, grdSegNet.model.input, satNet.model.input, satSegNet.model.input], 
-         outputs=[grdNet.model.output, grdSegNet.model.output,satNet.model.output, satSegNet.model.output]
+         outputs=[grdNet.model.output, grdSegNet.model.output, satNet.model.output, satSegNet.model.output]
     )
+    
     print("Model created")
 
     # The two halves of the model should have the same number of channels in total
-    assert grdNet.out_channels + grdSegNet.out_channels == satNet.out_channels + satSegNet.out_channels
     # otherwise the output feature maps will be of different sizes
+    if combination_type == 'concat':
+        assert grdNet.out_channels + grdSegNet.out_channels == satNet.out_channels + satSegNet.out_channels
+    elif combination_type == 'sum':
+        assert grdNet.out_channels == satNet.out_channels
+    else:
+        raise Exception("Combination method not implemented!")  
 
     # (empty) input tensors
     grd_x = np.float32(np.zeros([2, 128, width, 3]))
@@ -127,9 +167,16 @@ def train(start_epoch=14):
 
     # feature extraction and concatenation
     grd_features, grdseg_features, sat_features, satseg_features = model([grd_x, grdseg_x, polar_sat_x, satseg_x])
-    sat_features = tf.concat([sat_features, satseg_features], axis=-1)
-    grd_features = tf.concat([grd_features, grdseg_features], axis=-1)
-    
+    # feature extraction and concatenation
+    if combination_type == 'concat':
+        grd_features = tf.concat([grd_features, grdseg_features], axis=-1)                        
+        sat_features = tf.concat([sat_features, satseg_features], axis=-1)                        
+    elif combination_type == 'sum':
+        grd_features = tf.concat([tf.add(grd_features[:, :, :, :grdseg_c], grdseg_features), grd_features[:, :, :, grdseg_c:]], -1)
+        sat_features = tf.concat([tf.add(sat_features[:, :, :, :satseg_c], satseg_features), sat_features[:, :, :, satseg_c:]], -1)                        
+    else:
+        raise Exception("Combination method not implemented!")
+
     # computing the distance and the matching
     sat_matrix, grd_matrix, distance, pred_orien = processor.VGG_13_conv_v2_cir(sat_features,grd_features)
 
@@ -139,22 +186,23 @@ def train(start_epoch=14):
     sat_global_matrix = np.zeros([input_data.get_test_dataset_size(), s_height, s_width, s_channel])
     grd_global_matrix = np.zeros([input_data.get_test_dataset_size(), g_height, g_width, g_channel])
     orientation_gth = np.zeros([input_data.get_test_dataset_size()])
-    pred_orientation = np.zeros([input_data.get_test_dataset_size()])
 
-    # load Model
-    model_path = "./saved_models/path/epoch/"
-    model = keras.models.load_model(model_path)
-    print("Model checkpoint uploaded")
+    # load a Model
+    if start_epoch != 0:
+        model_path = "./saved_models/SANv2/18/"
+        model = keras.models.load_model(model_path)
+        print("Model checkpoint uploaded")
+
 
     print("Validation...")
     val_i = 0
     count = 0
     while True:
         # print('      progress %d' % val_i)
-
+        
         # take next batch
         batch_sat_polar, batch_sat, batch_grd, batch_satseg, batch_grdseg, batch_orien  = input_data.next_batch_scan(
-            8, 
+            batch_size, 
             grd_noise=test_grd_noise,
             FOV=test_grd_FOV
         )
@@ -166,10 +214,18 @@ def train(start_epoch=14):
         grd_features, grdseg_features, sat_features, satseg_features = model([batch_grd, batch_grdseg, batch_sat_polar, batch_satseg])
         
         # feature extraction and concatenation
-        ### ADD SMART COMBINATION THROUGH FULLY CONNECTED LAYER?            
-        sat_features = tf.concat([sat_features, satseg_features], axis=-1)
+        if combination_type == 'concat':
+            grd_features = tf.concat([grd_features, grdseg_features], axis=-1)                        
+            sat_features = tf.concat([sat_features, satseg_features], axis=-1)                        
+        elif combination_type == 'sum':
+            grdseg_c = grdseg_features.shape[-1]
+            satseg_c = grdseg_features.shape[-1]
+            grd_features = tf.concat([tf.add(grd_features[:, :, :, :grdseg_c], grdseg_features), grd_features[:, :, :, grdseg_c:]], -1)
+            sat_features = tf.concat([tf.add(sat_features[:, :, :, :satseg_c], satseg_features), sat_features[:, :, :, satseg_c:]], -1)                        
+        else:
+            raise Exception("Combination method not implemented!")
+
         grd_features = tf.nn.l2_normalize(grd_features, axis=[1, 2, 3])
-        grd_features = tf.concat([grd_features, grdseg_features], axis=-1)
         # sat_features is normalized after cropping
         
         # Compute correlation and distance matrix
@@ -183,6 +239,7 @@ def train(start_epoch=14):
         val_i += sat_matrix.shape[0]
         count += 1
 
+    
     file = './saved_models/path/filename.mat'
     scio.savemat(file, {'orientation_gth': orientation_gth, 'grd_descriptor': grd_global_matrix, 'sat_descriptor': sat_global_matrix})
     grd_descriptor = grd_global_matrix
@@ -193,12 +250,12 @@ def train(start_epoch=14):
     top1_percent = int(data_amount * 0.01) + 1
     print('      top1_percent %d' % top1_percent)
 
-    if test_grd_noise == 0:
+    if test_grd_noise == 0:  
 
         # at the end of accumulation reshape the feature maps into vectors (and normalize sat)
         sat_descriptor = np.reshape(sat_global_matrix[:, :, :g_width, :], [-1, g_height * g_width * g_channel])
         sat_descriptor = sat_descriptor / np.linalg.norm(sat_descriptor, axis=-1, keepdims=True)
-        grd_descriptor = np.reshape(grd_global_matrix, [-1, g_height * g_width * g_channel])
+        grd_descriptor = np.reshape(grd_global_matrix, [-1, g_height * g_width * g_channel]) 
         # the grd_descriptor is already normalized
 
         # compute distances
@@ -206,18 +263,22 @@ def train(start_epoch=14):
  
         # compute metrics
         val_top1 = validate(dist_array, 1)
-        print('top1 = %.2f%%' % (val_top1 * 100.0))
+        print('top1 = %.4f%%' % (val_top1 * 100.0))
         val_top5 = validate(dist_array, 5)
-        print('top5 = %.2f%%' % (val_top5 * 100.0))
+        print('top5 = %.4f%%' % (val_top5 * 100.0))
         val_top10 = validate(dist_array, 10)
-        print('top10 = %.2f%%' % (val_top10 * 100.0))
+        print('top10 = %.4f%%' % (val_top10 * 100.0))
         val_top1perc = validate(dist_array, top1_percent)
-        print('top1% = %.2f%%' % (val_top1perc * 100.0))
+        print('top1perc = %.4f%%' % (val_top1perc * 100.0))
+        val_loss = compute_loss(dist_array) / data_amount
+        print('loss (validation) = ' + format(val_loss.numpy(), '.8f'))
 
         gt_dist = dist_array.diagonal()
         prediction = np.sum(dist_array < gt_dist.reshape(-1, 1), axis=-1)
         loc_acc = np.sum(prediction.reshape(-1, 1) < np.arange(top1_percent), axis=0) / data_amount
-        scio.savemat(file, {'loc_acc': loc_acc, 'grd_descriptor': grd_descriptor, 'sat_descriptor': sat_descriptor}) 
-   
+        scio.savemat(file, {'loc_acc': loc_acc, 'grd_descriptor': grd_descriptor, 'sat_descriptor': sat_descriptor})            
+
+
+
 if __name__ == '__main__':
     train(start_epoch)
